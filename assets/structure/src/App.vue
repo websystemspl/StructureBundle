8b67d5d4d59fe4d317036f55a96498dd12d69bfe
Dariusz Ławiński <genosajd@gmail.com>
2024-04-14 08:01:43 +0200

    fix widgets load

--- assets/structure/src/App.vue
+++ assets/structure/src/App.vue
@@ -22,7 +22,7 @@ let props = defineProps({
     type: String,
   },
   widgets: {
-    type: Object,
+    type: String,
   },
 });
 
@@ -54,7 +54,7 @@ watch(() => contentStore.content, (newContent) => {
     <button type="button" class="start-structure__button" @click="startApp()">Start Structure</button>
   </div>
 
-  <loading :active="isLoading" :can-cancel="false" :is-full-page="fullPage" background-color="#000" opacity="1" transition="fade">
+  <loading :active="isLoading" :can-cancel="false" :is-full-page="true" background-color="#000" opacity="1" transition="fade">
     <template #default>
       <div class="loading-logo">Structure.</div>
     </template>
